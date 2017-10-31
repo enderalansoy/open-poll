2a488ab9edebb244a2a5ac1617fd0d98a9371e2d
Alan <aendersoy@gmail.com>
2017-10-31 14:02:08 +0300

    commitment

--- webpack/src/router/index.js
+++ webpack/src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import vSelect from 'vue-select';
+import { loadProgressBar } from 'axios-progress-bar';
+
 import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
+Vue.component('v-select', vSelect);
+
+loadProgressBar();
 
 export default new Router({
   routes: [
